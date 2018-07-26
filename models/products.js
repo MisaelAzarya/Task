1446fe690c8475a2d16708cde602ead52976b025
edwinx29 <edwinxkristian@gmail.com>
2018-07-26 13:33:22 +0700

    input barang #15

--- models/products.js
+++ models/products.js
@@ -6,6 +6,11 @@ var schema = new Schema({
   title: {type: String, required:true},
   description: {type: String, required:true},
   price: {type: Number, required:true},
+  color: {type: String, required: true},
+  brand: {type: String, required:true},
+  stock: {type: Number, required: true},
+  size: {type: Array, required: true},
+  gender: {type: String, required: true}
 });
 
 module.exports = mongoose.model('Product', schema);
